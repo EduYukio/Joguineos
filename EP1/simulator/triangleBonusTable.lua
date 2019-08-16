13c05111837feba98986be86f940956194a62b54
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-08-16 12:35:54 -0300

    Add triangleBonus data table

---
+++ EP1/simulator/triangleBonusTable.lua
@@ -0,0 +1,50 @@
+return {
+  sword = {
+    sword = 0,
+    axe = 1,
+    lance = -1,
+    wind = 0,
+    thunder = 0,
+    fire = 0
+  },
+  axe = {
+    sword = -1,
+    axe = 0,
+    lance = 1,
+    wind = 0,
+    thunder = 0,
+    fire = 0
+  },
+  lance = {
+    sword = 1,
+    axe = -1,
+    lance = 0,
+    wind = 0,
+    thunder = 0,
+    fire = 0
+  },
+  wind = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    wind = 0,
+    thunder = 1,
+    fire = -1
+  },
+  thunder = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    wind = -1,
+    thunder = 0,
+    fire = 1
+  },
+  fire = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    wind = 1,
+    thunder = -1,
+    fire = 0
+  }
+}
