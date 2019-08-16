f258f52c84bc264547e82e60f235e6e8a7bd9ba4
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-08-16 12:36:19 -0300

    Change main to act like a unit test file

--- EP1/simulator/main.lua
+++ EP1/simulator/main.lua
@@ -1,3 +1,7 @@
+-- luacheck: no global
+
+local simulator = require "simulator"
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  love.graphics.print(simulator.calculateTriangleBonus("sword", "lance"), 400, 300)
 end
