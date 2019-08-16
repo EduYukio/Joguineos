9351e3223398c97e33eceb25a5b93643de64e282
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-08-16 12:35:42 -0300

    Add "calculateTriangleBonus" function

--- EP1/simulator/simulator.lua
+++ EP1/simulator/simulator.lua
@@ -2,10 +2,46 @@
 local SIMULATOR = {}
 
 function SIMULATOR.run(scenario_input)
-  return scenario_input.units
+  local fights = scenario_input.fights
+
+  for attacker, defender in pairs(fights) do
+    -- local attacker = k
+    -- local defender = v
+    SIMULATOR.round(attacker, defender)
+  end
+
+  return scenario_input.output.units
 end
 
-result = SIMULATOR.run(scenario_input)
+function SIMULATOR.round(attacker, defender)
+  attacker:attack(defender)
+  defender:attack(attacker)
+end
+
+function SIMULATOR.attack(unit)
+end
+
+function SIMULATOR.calculateTriangleBonus(attackWeapon, defenseWeapon)
+  local t = require "triangleBonusTable"
+
+  return t[attackWeapon][defenseWeapon]
+end
+
+function SIMULATOR.attack()
+end
+
+function SIMULATOR.attack()
+end
+
+function SIMULATOR.attack()
+end
+
+
+
+
+
+
+-- result = SIMULATOR.run(scenario_input)
 
 return SIMULATOR
 
