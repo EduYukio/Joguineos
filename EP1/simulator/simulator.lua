b2cc61becc3b6a584cce336ac1ba95cb86c8b710
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-08-16 12:58:31 -0300

    Add calculateHitChance function

--- EP1/simulator/simulator.lua
+++ EP1/simulator/simulator.lua
@@ -18,28 +18,35 @@ function SIMULATOR.round(attacker, defender)
   defender:attack(attacker)
 end
 
-function SIMULATOR.attack(unit)
+-- function SIMULATOR.attack(unit)
+function SIMULATOR.attack()
 end
 
 function SIMULATOR.calculateTriangleBonus(attackWeapon, defenseWeapon)
-  local t = require "triangleBonusTable"
+  local table = require "triangleBonusTable"
 
-  return t[attackWeapon][defenseWeapon]
+  return table[attackWeapon][defenseWeapon]
 end
 
-function SIMULATOR.attack()
+function SIMULATOR.calculateAttackSpeed(unit)
+  return unit.spd - math.max(0, unit.wt - unit.str)
 end
 
-function SIMULATOR.attack()
+function SIMULATOR.calculateHitChance(attacker, defender)
+  local attackerWeapon = attacker.weapons[attacker.weapon]
+  local defenderWeapon = defender.weapons[attacker.weapon]
+
+  local defenderAttackSpeed = SIMULATOR.calculateAttackSpeed(defender)
+  local triangleBonus = SIMULATOR.calculateTriangleBonus(attackerWeapon,
+                        defenderWeapon)
+
+  local acc = attackerWeapon.hit + attacker.skl*2 +
+              attacker.lck + triangleBonus*10
+
+  local avo = (defenderAttackSpeed*2) + defender.lck
+
+  return math.max(0, math.min(100, acc - avo))
 end
-
-function SIMULATOR.attack()
-end
-
-
-
-
-
 
 -- result = SIMULATOR.run(scenario_input)
 
