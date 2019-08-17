5170fec82f62ca32829665f46fd097404d6b9170
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-08-17 00:45:32 -0300

    Reorganize triangleTable

--- EP1/simulator/triangleBonusTable.lua
+++ EP1/simulator/triangleBonusTable.lua
@@ -3,63 +3,63 @@ return {
     sword = 0,
     axe = 1,
     lance = -1,
+    bow = 0,
     wind = 0,
     thunder = 0,
-    fire = 0,
-    bow = 0
+    fire = 0
   },
   axe = {
     sword = -1,
     axe = 0,
     lance = 1,
+    bow = 0,
     wind = 0,
     thunder = 0,
-    fire = 0,
-    bow = 0
+    fire = 0
   },
   lance = {
     sword = 1,
     axe = -1,
     lance = 0,
+    bow = 0,
     wind = 0,
     thunder = 0,
-    fire = 0,
-    bow = 0
-  },
-  wind = {
-    sword = 0,
-    axe = 0,
-    lance = 0,
-    wind = 0,
-    thunder = 1,
-    fire = -1,
-    bow = 0
-  },
-  thunder = {
-    sword = 0,
-    axe = 0,
-    lance = 0,
-    wind = -1,
-    thunder = 0,
-    fire = 1,
-    bow = 0
-  },
-  fire = {
-    sword = 0,
-    axe = 0,
-    lance = 0,
-    wind = 1,
-    thunder = -1,
-    fire = 0,
-    bow = 0
+    fire = 0
   },
   bow = {
     sword = 0,
     axe = 0,
     lance = 0,
+    bow = 0,
     wind = 0,
     thunder = 0,
-    fire = 0,
-    bow = 0
+    fire = 0
+  },
+  wind = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    bow = 0,
+    wind = 0,
+    thunder = 1,
+    fire = -1,
+  },
+  thunder = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    bow = 0,
+    wind = -1,
+    thunder = 0,
+    fire = 1
+  },
+  fire = {
+    sword = 0,
+    axe = 0,
+    lance = 0,
+    bow = 0,
+    wind = 1,
+    thunder = -1,
+    fire = 0
   }
 }
