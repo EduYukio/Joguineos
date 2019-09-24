1eb1dda54dd5ecc5a2bcd547bfe07ecfc9996944
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-24 17:21:44 -0300

    Add default values of entities

---
+++ EP3/entity/default.lua
@@ -0,0 +1,23 @@
+local Vec = require 'class' ()
+
+return {
+  position = {
+    point = Vec(0, 0)
+  },
+  movement = {
+    motion = Vec(0, 0)
+  },
+  body = {
+    size = 8
+  },
+  control = {
+    acceleration = 0.0,
+    max_speed = 50.0,
+  },
+  field = {
+    strength = 1
+  },
+  charge = {
+    strength = 1
+  }
+}
