cbb9fc56252b8ed073b70eaaab59424c283c65f2
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-26 19:10:59 -0300

    Add conf file setting the window resolution

---
+++ EP3/conf.lua
@@ -0,0 +1,17 @@
+-- luacheck: globals love
+
+function love.conf(t)
+  t.window.title = "EP3-Yukio-MAC0346"
+  t.window.width = 1280
+  t.window.height = 600
+
+  -- t.modules.joystick = false
+  -- t.modules.keyboard = false
+  -- t.modules.physics = false
+  -- t.modules.system = false
+  -- t.modules.audio = false
+  -- t.modules.mouse = false
+  -- t.modules.sound = false
+  -- t.modules.touch = false
+  -- t.modules.video = false
+end
