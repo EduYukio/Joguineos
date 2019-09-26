bd59f6ebc729354f74ec7c6a58d2e800a11ff10a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-26 17:38:12 -0300

    Add the instantiation of all entities of the scene

--- EP3/main.lua
+++ EP3/main.lua
@@ -1,8 +1,30 @@
---luacheck: globals love
+--luacheck: globals love class
+
+class = require "class"
+
+class.Entity()
+
+function Entity:_init(name, properties)
+  self.name = name
+  self.properties = properties
+end
 
 local scene
-
+local entities = {}
 
 function love.load(args)
   scene = love.filesystem.load("scene/".. args[1] ..".lua")()
+
+  for i = 1, #scene do
+    for _ = 1, scene[i].n do
+      local objName = scene[i].entity
+      local object = Entity(objName, require("entity/" .. objName))
+      table.insert(entities, object)
+    end
+  end
+end
+
+function love.update(dt)
+  print(entities[1].name)
+  print(entities[1].properties.control.acceleration)
 end
