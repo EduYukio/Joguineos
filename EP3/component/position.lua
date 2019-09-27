aaf803a913a6b5645969567447e5c971ba49f66e
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-27 18:09:19 -0300

    Add drawing of player and entities at random places

--- EP3/component/position.lua
+++ EP3/component/position.lua
@@ -1,11 +1,22 @@
 --luacheck: globals love class
 
-
 class = require "class"
 class.Position()
 
-function Position:_init() -- luacheck: ignore
-  -- self.name = name
+local Vec = require 'common/vec'
+
+function Position:_init(point) -- luacheck: ignore
+  if not point then
+    -- self.point = Vec(0,0)
+    local randomX = math.random(-900, 900)
+
+    local maxY = math.sqrt(900^2 - randomX^2)
+    local randomY = math.random(-maxY, maxY)
+
+    self.point = Vec(randomX, randomY)
+  else
+    self.point = point
+  end
 end
 
 function Position:update() -- luacheck: ignore
--- EP3/main.lua
+++ EP3/main.lua
@@ -5,22 +5,45 @@ local entities = {}
 
 class = require "class"
 
+require "component/position"
+require "component/movement"
+require "component/body"
+require "component/control"
+require "component/field"
+require "component/charge"
+
 class.Entity()
 
 function Entity:_init(name, initialState) -- luacheck: ignore
   self.name = name
   self.initialState = initialState
 
-  self.position = nil
+  local position = initialState.position
+  if not position then
+    self.position = nil
+  else
+    self.position = Position(position.point) -- luacheck: ignore
+  end
+
   self.movement = nil
   self.body = nil
+
+  local control = initialState.control
+  if not control then
+    self.control = nil
+  else
+    self.control = Control(control.acceleration, control.max_speed) -- luacheck: ignore
+  end
+
+
   self.field = nil
   self.charge = nil
-  self.control = nil
 end
 
 function love.load(args)
-  scene = love.filesystem.load("scene/".. args[1] ..".lua")()
+  math.randomseed(os.time())
+
+  scene = love.filesystem.load("scene/" .. args[1] .. ".lua")()
 
   for i = 1, #scene do
     for _ = 1, scene[i].n do
@@ -32,18 +55,36 @@ function love.load(args)
   end
 end
 
-function love.update(dt)
+function love.update(_)
   for _, entity in ipairs(entities) do
     if entity.position then entity.position:update() end
     if entity.movement then entity.movement:update() end
-    if entity.body     then entity.body:update() end
-    if entity.field    then entity.field:update() end
-    if entity.charge   then entity.charge:update() end
-    if entity.control  then entity.control:update() end
+    if entity.body     then entity.body:update()     end
+    if entity.control  then entity.control:update()  end
+    if entity.field    then entity.field:update()    end
+    if entity.charge   then entity.charge:update()   end
   end
 end
 
 function love.draw()
   love.graphics.translate(1280/2, 600/2)
+
+  -- love.graphics.scale(0.3,0.3)
+
+  love.graphics.setColor(1, 1, 1)
   love.graphics.circle("line", 0, 0, 1000)
+
+  for _, entity in ipairs(entities) do
+    if(entity.name == "player") then
+      love.graphics.setColor(1, 1, 1)
+      love.graphics.polygon('fill', -3, -4, 3, -4, 0, 6)
+
+      -- love.graphics.setColor(0, 1, 0)
+      -- love.graphics.circle("line", 0, 0, 8)
+    else
+      love.graphics.setColor(0, 1, 0)
+      local xPos, yPos = entity.position.point:get()
+      love.graphics.circle("line", xPos, yPos, 8)
+    end
+  end
 end
