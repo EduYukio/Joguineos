1efb5479a2722aba2f03c3ccf022bdd018ec2f43
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-28 17:09:59 -0300

    Add input handling and velocity calculation

--- EP3/component/control.lua
+++ EP3/component/control.lua
@@ -4,6 +4,8 @@
 class = require "class"
 class.Control()
 
+local Vec = require 'common/vec'
+
 function Control:_init(acceleration, max_speed) -- luacheck: ignore
   if not acceleration then
     self.acceleration = 0.0
@@ -18,5 +20,35 @@ function Control:_init(acceleration, max_speed) -- luacheck: ignore
   end
 end
 
-function Control:update() -- luacheck: ignore
+local direction
+local function handleInput()
+  direction = Vec(0,0)
+
+  if love.keyboard.isDown("up") then
+    direction.y = -1
+  end
+
+  if love.keyboard.isDown("down") then
+    direction.y = 1
+  end
+
+  if love.keyboard.isDown("left") then
+    direction.x = -1
+  end
+
+  if love.keyboard.isDown("right") then
+    direction.x = 1
+  end
+
+  return direction
+end
+
+function Control:update(dt, v0) -- luacheck: ignore
+  local i = handleInput()
+
+  local v = i*(self.acceleration*dt) + v0
+
+  v:clamp(self.max_speed)
+
+  return v
 end
