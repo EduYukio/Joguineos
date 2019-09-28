921507c172f7712425d086b276aa2226618ff4ef
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-28 17:12:12 -0300

    Add component communication by having the entity being the mediator

--- EP3/main.lua
+++ EP3/main.lua
@@ -4,6 +4,7 @@ local scene
 local entities = {}
 
 class = require "class"
+local Vec = require 'common/vec'
 
 require "component/position"
 require "component/movement"
@@ -21,11 +22,19 @@ function Entity:_init(name, initialState) -- luacheck: ignore
   local position = initialState.position
   if not position then
     self.position = nil
+  elseif name == "player" then
+    self.position = Position(Vec(0,0)) -- luacheck: ignore
   else
     self.position = Position(position.point) -- luacheck: ignore
   end
 
-  self.movement = nil
+  local movement = initialState.movement
+  if not movement then
+    self.movement = nil
+  else
+    self.movement = Movement(movement.motion) -- luacheck: ignore
+  end
+
   self.body = nil
 
   local control = initialState.control
@@ -34,7 +43,6 @@ function Entity:_init(name, initialState) -- luacheck: ignore
   else
     self.control = Control(control.acceleration, control.max_speed) -- luacheck: ignore
   end
-
 
   self.field = nil
   self.charge = nil
@@ -55,12 +63,33 @@ function love.load(args)
   end
 end
 
-function love.update(_)
+function love.update(dt)
   for _, entity in ipairs(entities) do
-    if entity.position then entity.position:update() end
-    if entity.movement then entity.movement:update() end
+    local updatedMotion
+    local updatedPosition
+    local currentMotion
+    local currentPosition
+
     if entity.body     then entity.body:update()     end
-    if entity.control  then entity.control:update()  end
+
+    if entity.movement then
+      currentMotion = entity.movement.motion
+    end
+
+    if entity.control then
+      updatedMotion = entity.control:update(dt, currentMotion)
+    end
+
+    if entity.position then
+      currentPosition = entity.position.point
+    end
+
+    if entity.movement then
+      updatedPosition = entity.movement:update(dt, updatedMotion, currentPosition)
+    end
+
+    if entity.position then entity.position:update(updatedPosition) end
+
     if entity.field    then entity.field:update()    end
     if entity.charge   then entity.charge:update()   end
   end
@@ -77,10 +106,8 @@ function love.draw()
   for _, entity in ipairs(entities) do
     if(entity.name == "player") then
       love.graphics.setColor(1, 1, 1)
-      love.graphics.polygon('fill', -3, -4, 3, -4, 0, 6)
-
-      -- love.graphics.setColor(0, 1, 0)
-      -- love.graphics.circle("line", 0, 0, 8)
+      local x, y = entity.position.point:get()
+      love.graphics.polygon('fill', x-3, y-4, x+3, y-4, x+0, y+6)
     else
       love.graphics.setColor(0, 1, 0)
       local xPos, yPos = entity.position.point:get()
