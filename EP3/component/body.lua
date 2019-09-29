47a1fafeb3d9f03fb719bcc469e23d45142cb917
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-29 15:49:03 -0300

    Add collision handling on body component

--- EP3/component/body.lua
+++ EP3/component/body.lua
@@ -2,11 +2,35 @@
 
 local Body = require "class"()
 
-function Body:_init()
-  self.size = 8
+function Body:_init(size)
+	if not size then
+	  self.size = 8
+	else
+	  self.size = size
+	end
 end
 
-function Body:update()
+local function handleCollision(entity, distance, sumOfRadiuses)
+  local intersectionLength = sumOfRadiuses - distance:length()
+  local outDirection = distance:normalized()
+
+  local x2 = entity.position.point
+  entity.position.point = x2 + outDirection*intersectionLength
+
+  local v2 = entity.movement.motion
+  entity.movement.motion = v2 - (outDirection*v2)*outDirection
+end
+
+function Body:update(entity, entities)
+	for _, other in ipairs(entities) do
+	  local distance = entity.position.point - other.position.point
+	  local sumOfRadiuses = self.size + other.body.size
+
+	  if distance:length() < sumOfRadiuses then
+	    handleCollision(entity, distance, sumOfRadiuses)
+	    -- return?
+	  end
+	end
 end
 
 return Body
