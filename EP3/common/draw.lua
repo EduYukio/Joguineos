1e1be26ff8950fa615c31aaf09e9920fdbe7940d
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-29 20:32:52 -0300

    Refactor love.draw

---
+++ EP3/common/draw.lua
@@ -0,0 +1,36 @@
+--luacheck: globals love
+
+local Draw = {}
+
+local Calculate = require "common/calculate"
+local pointingAngle = 0
+
+local function rotateToFaceDirection(x, y, direction)
+  pointingAngle = Calculate.pointingAngle(direction, pointingAngle)
+  love.graphics.translate(x - 1.5, y - 2)
+  love.graphics.rotate(pointingAngle)
+  love.graphics.translate(-x + 1.5, -y + 2)
+end
+
+function Draw.player(x, y, direction)
+  local triangleVertexes = {x - 3, y - 4, x + 3, y - 4, x + 0, y + 6}
+
+  love.graphics.push()
+
+  rotateToFaceDirection(x, y, direction)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.polygon('fill', triangleVertexes)
+
+  love.graphics.pop()
+end
+
+function Draw.nonPlayerEntities(entity, x, y)
+  love.graphics.setColor(0, 1, 0)
+  if entity.body then
+    love.graphics.circle("fill", x, y, entity.body.size)
+  else
+    love.graphics.circle("line", x, y, 8)
+  end
+end
+
+return Draw
--- EP3/main.lua
+++ EP3/main.lua
@@ -3,11 +3,10 @@
 local InputHandler = require "component/inputHandler"
 local Entity = require "entity/entity"
 
-local Calculate = require "common/calculate"
 local Update = require "common/update"
 local Get = require "common/get"
+local Draw = require "common/draw"
 
-local pointingAngle = 0
 local entities = {}
 local direction
 local scene
@@ -42,34 +41,6 @@ function love.update(dt)
   end
 end
 
-local function rotateToFaceDirection(x, y)
-  pointingAngle = Calculate.pointingAngle(direction, pointingAngle)
-  love.graphics.translate(x - 1.5, y - 2)
-  love.graphics.rotate(pointingAngle)
-  love.graphics.translate(-x + 1.5, -y + 2)
-end
-
-local function drawPlayer(x, y)
-  local triangleVertexes = {x - 3, y - 4, x + 3, y - 4, x + 0, y + 6}
-
-  love.graphics.push()
-
-  rotateToFaceDirection(x, y)
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.polygon('fill', triangleVertexes)
-
-  love.graphics.pop()
-end
-
-local function drawNonPlayerEntities(entity, x, y)
-  love.graphics.setColor(0, 1, 0)
-  if entity.body then
-    love.graphics.circle("fill", x, y, entity.body.size)
-  else
-    love.graphics.circle("line", x, y, 8)
-  end
-end
-
 function love.draw()
   local screenWidth, screenHeight = love.graphics.getDimensions()
   love.graphics.translate(screenWidth/2, screenHeight/2)
@@ -78,9 +49,9 @@ function love.draw()
     local x, y = entity.position.point:get()
     if entity.name == "player" then
       love.graphics.translate(-x, -y)
-      drawPlayer(x, y)
+      Draw.player(x, y, direction)
     else
-      drawNonPlayerEntities(entity, x, y)
+      Draw.nonPlayerEntities(entity, x, y)
     end
   end
 
