a35ae0d381791704720ce3162c320df8348b62a1
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-09-29 18:23:11 -0300

    Disable unused love modules

--- EP3/conf.lua
+++ EP3/conf.lua
@@ -5,13 +5,12 @@ function love.conf(t)
   t.window.width = 1280
   t.window.height = 600
 
-  -- t.modules.joystick = false
-  -- t.modules.keyboard = false
-  -- t.modules.physics = false
-  -- t.modules.system = false
-  -- t.modules.audio = false
-  -- t.modules.mouse = false
-  -- t.modules.sound = false
-  -- t.modules.touch = false
-  -- t.modules.video = false
+  t.modules.joystick = false
+  t.modules.physics = false
+  t.modules.system = false
+  t.modules.mouse = false
+  t.modules.audio = false
+  t.modules.sound = false
+  t.modules.touch = false
+  t.modules.video = false
 end
