99e5de464e5e66a69fecaf8c57e3c2d4948488fa
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-11-03 20:38:39 -0300

    Add unit properties file

---
+++ TowerDefense/database/properties.lua
@@ -0,0 +1,46 @@
+
+return {
+  damage = {
+    none = 0,
+    low = 1/60,
+    medium = 4/60,
+    high = 10/60,
+    very_high = 15/60,
+  },
+  target = {
+    none = 0,
+    single = 1,
+    aoe = 3,
+  },
+  range = {
+    one = 32*1,
+    low = 32*2,
+    medium = 32*5,
+    high = 32*10,
+  },
+  slow_factor = {
+    medium = 1.5,
+    high = 3,
+  },
+  speed = {
+    none = 0,
+    low = 10,
+    medium = 20,
+    high = 50,
+  },
+  hp = {
+    low = 2,
+    medium = 5,
+    high = 10,
+  },
+  castle_hp = {
+    normal = 5,
+    upgraded = 10,
+  },
+  dmg_buff_factor = 1.25,
+  gold_to_produce = 1,
+  blink_distance = 32*3,
+  blink_delay = 1.5,
+  summon_delay = 3.5,
+}
+
