9b5a0a414e58d8dcd4c16739cf93945c26c81a07
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-11-04 03:24:36 -0300

    Change waves to better test the game

--- TowerDefense/database/stages/slime_infestation.lua
+++ TowerDefense/database/stages/slime_infestation.lua
@@ -3,13 +3,13 @@ return {
   title = 'Slime Infestation',
   waves = {
     { order = {"slime"},
-      quantity = { 2 },
+      quantity = {2},
+    },
+    { order = {"golem", "bat"},
+      quantity = {5, 15},
     },
     { order = {"slime", "bat", "slime", "golem", "blinker", "summoner"},
-      quantity = {2, 2, 3, 1, 1, 1},
-    },
-    { order = {"slime"},
-      quantity = { 4 },
+      quantity = {2, 2, 3, 2, 1, 1},
     },
   }
 }
