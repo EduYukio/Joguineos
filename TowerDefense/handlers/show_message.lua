396192c36b70880835fc9c04987139d82506092d
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-11-10 17:26:05 -0300

    Add "tutorial" (first playthrough takes 30 seconds to spawn 1st wave)

--- TowerDefense/handlers/show_message.lua
+++ TowerDefense/handlers/show_message.lua
@@ -5,11 +5,16 @@ function ShowMessage:_init(stage)
   self.waiting_time = 0
   self.stage = stage
   self.messages = self.stage.messages
+  if not self.stage.done_tutorial then
+    self.time_to_wait = 30
+    self.stage.done_tutorial = true
+  else
+    self.time_to_wait = 3
+  end
 end
 
 function ShowMessage:next_wave(dt)
   self.waiting_time = self.waiting_time + dt
-  local time_to_wait = 5
 
   if self.waiting_time < self.time_to_wait then
     local seconds = tostring(self.time_to_wait - math.floor(self.waiting_time))
