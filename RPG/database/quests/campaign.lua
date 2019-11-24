ec6be26e38b76834b2f1fd740677511a07a660e1
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-11-23 21:51:54 -0300

    Change quest to fit the new monsters

---
+++ RPG/database/quests/campaign.lua
@@ -0,0 +1,11 @@
+
+return {
+  title = 'Campaign',
+  party = { 'knight', 'archer', 'priest' },
+  encounters = {
+    { 'slime', 'golem', 'ninja' },
+    { 'slime', 'golem', 'ninja', 'ninja' },
+    { 'slime', 'golem', 'ninja', 'golem' },
+  }
+}
+
