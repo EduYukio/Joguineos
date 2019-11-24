601f4ab8be90be5dd6eaad4aa8449cc7ccb33681
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-11-24 03:58:34 -0300

    Add information about the players into the UI

--- RPG/view/character_stats.lua
+++ RPG/view/character_stats.lua
@@ -1,22 +1,34 @@
 
+local g = love.graphics
+
 local CharacterStats = require 'common.class' ()
 
 function CharacterStats:_init(position, character)
   self.position = position
   self.character = character
-  self.font = love.graphics.newFont('assets/fonts/VT323-Regular.ttf', 36)
-  self.font:setFilter('nearest', 'nearest')
+  self.title_font = g.newFont('assets/fonts/VT323-Regular.ttf', 36)
+  self.text_font = g.newFont('assets/fonts/VT323-Regular.ttf', 24)
+  self.title_font:setFilter('nearest', 'nearest')
+  self.text_font:setFilter('nearest', 'nearest')
 end
 
 function CharacterStats:draw()
-  local g = love.graphics
   g.push()
-  g.setFont(self.font)
+
   g.setColor(1, 1, 1)
   g.translate(self.position:get())
+
+  g.setFont(self.title_font)
   g.print(self.character.name)
-  g.translate(0, self.font:getHeight())
-  g.print(("HP: %d/%d"):format(self.character.hp, self.character.max_hp))
+
+  g.translate(0, self.title_font:getHeight())
+  g.setFont(self.text_font)
+
+  local line_gap = 20
+  g.print(("DMG: %d"):format(self.character.damage))
+  g.print(("EVA: %d%%"):format(self.character.evasion*100), 0, line_gap)
+  g.print(("CRI: %d%%"):format(self.character.crit_chance*100), 0, line_gap*2)
+
   g.pop()
 end
 
